f65cb9439d8e59dc67f062a786a993b2633c2223
Chiranjeev Kailash Chand <ckailashchand@munichre.com>
2025-07-20 09:50:04 +0530

    Second Version of Data Saved

--- mycode.py
+++ mycode.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import os
 
-data = {'Name':['Chiranjeev'],
-'Age':[23],
-'City' :['Delhi']}
+data = {'Name':['Chiranjeev','Rajat'],
+'Age':[23,25],
+'City' :['Delhi','Rewari']}
 
 df = pd.DataFrame(data)
 
-
-
-
+new_row = {'Name':'Amit','Age':30,'City':'Mumbai'}
+df.loc[len(df)] = new_row
 data_dir = 'data'
-os.makedirs(data_dir,exist_ok = True)
 
+os.makedirs(data_dir,exist_ok = True)
 file_path = os.path.join(data_dir,'sample_data.csv')
 
 df.to_csv(file_path,index=False)
-
 print(f"CSV file saved to {file_path}")
